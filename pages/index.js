2041885c0ec0f824d2d43bd9ab458fe3db080f2a
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-17 11:52:17 -0300

    criando e utilizando req dos pokemons (básico)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+
+  const maxPokemons = 251
+
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+
+  const data = await res.json()
+
+  //adicionar id do pokemon
+  data.results.forEach((element, index) => {
+    element.id = index + 1
+  });
+
+
+  return{
+    props: {
+      pokemons: data.results
+    }
+  }
+
+}
+
+export default function Home({pokemons}) {
   return (
-    <>
+    <div>
       <h1>PokeNext</h1>
-    </>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
